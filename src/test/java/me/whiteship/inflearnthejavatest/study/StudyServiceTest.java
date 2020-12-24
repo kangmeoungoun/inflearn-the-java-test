952a90fe5c55b197ec8fbf7b6e264f19aa48de01
kangmeoungoun <kes98202@naver.com>
2020-12-24 09:26:41 +0900

    junit 5 Mock 객체 Stubbing

--- src/test/java/me/whiteship/inflearnthejavatest/study/StudyServiceTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/study/StudyServiceTest.java
@@ -33,8 +33,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest{
@@ -45,12 +44,45 @@ class StudyServiceTest{
     //StudyRepository studyRepository;
 
     @Test
-    void createStudyService(@Mock MemberService memberService
+    void createNewStudy(@Mock MemberService memberService
             , @Mock StudyRepository studyRepository){
-        //MemberService memberService = mock(MemberService.class);
-        //StudyRepository studyRepository = mock(StudyRepository.class);
 
         StudyService studyService = new StudyService(memberService , studyRepository);
         assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("kes98202@naver.com");
+
+        /*
+        //2L 로 호출할시 객체 못받는다.
+        //when(memberService.findById(1L)).thenReturn(Optional.of(member));
+        //파라미터러 멀 호출하던간에 같은 객체롤 호출
+        when(memberService.findById(any())).thenReturn(Optional.of(member));
+
+        assertEquals("kes98202@naver.com",memberService.findById(1L).get().getEmail());
+        assertEquals("kes98202@naver.com",memberService.findById(2L).get().getEmail());
+
+        doThrow(new IllegalArgumentException()).when(memberService).validate(1l);
+        assertThrows(IllegalArgumentException.class,()->{
+            memberService.validate(1l);
+        });
+        memberService.validate(2l);
+
+        when(memberService.findById(1L)).thenThrow(new RuntimeException());
+        */
+        when(memberService.findById(any())).thenReturn(Optional.of(member))
+                                            .thenThrow(new RuntimeException())
+                                            .thenReturn(Optional.empty());
+
+        Optional<Member> byId = memberService.findById(1L);
+        assertEquals("kes98202@naver.com",byId.get().getEmail());
+
+        assertThrows(RuntimeException.class,()->{
+            memberService.findById(2L);
+        });
+
+        assertEquals(Optional.empty(),memberService.findById(3L));
+
     }
 }
